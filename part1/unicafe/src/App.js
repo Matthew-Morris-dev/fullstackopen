6d1061e687803c419a6255e562b5cb9c53e69e38
Matthew Morris <43638221+Matthew-Morris-dev@users.noreply.github.com>
2021-03-22 22:33:18 +0000

    exercise 1.8

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,5 +1,19 @@
 import React, { useState } from "react";
 
+const Statistics = (props) => {
+    return (
+        <div>
+            <h1>statistics</h1>
+            <p>good {props.stats.good}</p>
+            <p>neutral {props.stats.neutral}</p>
+            <p>bad {props.stats.bad}</p>
+            <p>all {props.stats.all}</p>
+            <p>average {props.stats.average}</p>
+            <p>positive {props.stats.positive} %</p>
+        </div>
+    );
+};
+
 const App = () => {
     // save clicks of each button to its own state
     const [good, setGood] = useState(0);
@@ -19,19 +33,22 @@ const App = () => {
         return good / allFeedback();
     };
 
+    const stats = {
+        good: good,
+        neutral: neutral,
+        bad: bad,
+        all: allFeedback(),
+        average: averageFeedback(),
+        positive: positiveFeedback()
+    };
+
     return (
         <div>
             <h1>give feedback</h1>
             <button onClick={increaseGood}>good</button>
             <button onClick={increaseNeutral}>neutral</button>
             <button onClick={increaseBad}>bad</button>
-            <h1>statistics</h1>
-            <p>good {good}</p>
-            <p>neutral {neutral}</p>
-            <p>bad {bad}</p>
-            <p>all {allFeedback()}</p>
-            <p>average {averageFeedback()}</p>
-            <p>positive {positiveFeedback()} %</p>
+            <Statistics stats={stats} />
         </div>
     );
 };
