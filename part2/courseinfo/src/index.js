2a05be3b6edf7c6f4fec6085ce82fc266aaeb98a
Matthew Morris <43638221+Matthew-Morris-dev@users.noreply.github.com>
2021-03-24 22:14:04 +0000

    exercise 2.1

---
+++ part2/courseinfo/src/index.js
@@ -0,0 +1,78 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Header = ({ course }) => {
+    return <h1>{course.name}</h1>;
+};
+
+const Total = ({ course }) => {
+    const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises;
+    return <p>Number of exercises {sum}</p>;
+};
+
+const Part = ({ part }) => {
+    console.log(part.name + " | " + part.exercises);
+    return (
+        <p>
+            {part.name} {part.exercises}
+        </p>
+    );
+};
+
+const Content = ({ course }) => {
+    console.log(course);
+    const results = course.parts.map((part) => {
+        return <Part key={part.id} part={part} />;
+    });
+    console.log(results);
+    return (
+        <div>
+            {course.parts.map((part) => {
+                return <Part key={part.id} part={part} />;
+            })}
+        </div>
+    );
+};
+
+const Course = ({ course }) => {
+    console.log(course.parts);
+    return (
+        <div>
+            <Header course={course} />
+            <Content course={course} />
+            {/* <Total course={course} /> */}
+        </div>
+    );
+};
+
+const App = () => {
+    const course = {
+        id: 1,
+        name: "Half Stack application development",
+        parts: [
+            {
+                name: "Fundamentals of React",
+                exercises: 10,
+                id: 1
+            },
+            {
+                name: "Using props to pass data",
+                exercises: 7,
+                id: 2
+            },
+            {
+                name: "State of a component",
+                exercises: 14,
+                id: 3
+            }
+        ]
+    };
+
+    return (
+        <div>
+            <Course course={course} />
+        </div>
+    );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
