c7293259bcb20e817094d682b9a69c31fab4ef8b
Matthew Morris <43638221+Matthew-Morris-dev@users.noreply.github.com>
2021-03-24 22:28:45 +0000

    exercise 2.5

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -1,45 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-const Header = ({ course }) => {
-    return <h1>{course.name}</h1>;
-};
-
-const Total = ({ parts }) => {
-    const sum = parts.reduce((acc, part) => {
-        return acc + part.exercises;
-    }, 0);
-    return <p>Number of exercises {sum}</p>;
-};
-
-const Part = ({ part }) => {
-    return (
-        <p>
-            {part.name} {part.exercises}
-        </p>
-    );
-};
-
-const Content = ({ parts }) => {
-    return (
-        <div>
-            {parts.map((part) => {
-                return <Part key={part.id} part={part} />;
-            })}
-        </div>
-    );
-};
-
-const Course = ({ course }) => {
-    console.log(course.parts);
-    return (
-        <div>
-            <Header course={course} />
-            <Content parts={course.parts} />
-            <Total parts={course.parts} />
-        </div>
-    );
-};
+import Course from "./components/Course";
 
 const App = () => {
     const courses = [
