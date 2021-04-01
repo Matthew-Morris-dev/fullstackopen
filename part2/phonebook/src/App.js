1aad129971dba2c3bdf32b711d5e246e22c7efa7
Matthew Morris <43638221+Matthew-Morris-dev@users.noreply.github.com>
2021-04-02 00:37:30 +0100

    exercise 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,13 +1,15 @@
 import React, { useState } from "react";
 
 const App = () => {
-    const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+    const [persons, setPersons] = useState([{ name: "Arto Hellas", number: "+44 0000 111 222" }]);
     const [newName, setNewName] = useState("");
+    const [newNumber, setNewNumber] = useState("");
 
     const addPerson = (event) => {
         event.preventDefault();
         const newPerson = {
-            name: newName
+            name: newName,
+            number: newNumber
         };
 
         if (persons.some((person) => person.name === newName)) {
@@ -23,6 +25,10 @@ const App = () => {
         setNewName(event.target.value);
     };
 
+    const handleNumberChange = (event) => {
+        setNewNumber(event.target.value);
+    };
+
     return (
         <div>
             <h2>Phonebook</h2>
@@ -31,13 +37,18 @@ const App = () => {
                     name: <input value={newName} onChange={handleNameChange} />
                 </div>
                 <div>
+                    number: <input value={newNumber} onChange={handleNumberChange} />
+                </div>
+                <div>
                     <button type="submit">add</button>
                 </div>
             </form>
             <h2>Numbers</h2>
             <ul>
                 {persons.map((person) => (
-                    <li key={person.name}>{person.name}</li>
+                    <li key={person.name}>
+                        {person.name} {person.number}
+                    </li>
                 ))}
             </ul>
         </div>
