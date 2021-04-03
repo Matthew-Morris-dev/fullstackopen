0901ae6bd346a44206d71a46c39e14f9585fff73
Matthew Morris <43638221+Matthew-Morris-dev@users.noreply.github.com>
2021-04-03 21:37:03 +0100

    exercise 2.16

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import Filter from "./components/Filter";
 import PersonForm from "./components/PersonForm";
 import Persons from "./components/Persons";
+import personsService from "./services/persons";
 
 const App = () => {
     const [persons, setPersons] = useState([]);
@@ -11,8 +11,8 @@ const App = () => {
     const [newFilter, setNewFilter] = useState("");
 
     const getPhonebook = () => {
-        axios.get("http://localhost:3001/persons").then((response) => {
-            setPersons(response.data);
+        personsService.getAll().then((returnedPersons) => {
+            setPersons(returnedPersons);
         });
     };
 
@@ -30,8 +30,8 @@ const App = () => {
             return;
         }
 
-        axios.post("http://localhost:3001/persons", newPerson).then((response) => {
-            setPersons(persons.concat(response.data));
+        personsService.create(newPerson).then((returnedPerson) => {
+            setPersons(persons.concat(returnedPerson));
             setNewName("");
             setNewNumber("");
         });
