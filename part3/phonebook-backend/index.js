4685e1ec7b89f9a2f44e70d9542dc2b25c82887e
Matthew Morris <m.morris9491@gmail.com>
2021-05-18 23:15:13 +0100

    exercise 3.4

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -41,6 +41,21 @@ app.get("/api/persons/:id", (req, res) => {
     }
 });
 
+app.delete("/api/persons/:id", (req, res) => {
+    const id = Number(req.params.id);
+    const person = persons.find((person) => {
+        return person.id === id;
+    });
+
+    if (person === undefined || person === null) {
+        res.sendStatus(404);
+    } else {
+        const index = persons.indexOf(person);
+        persons.splice(index, 1);
+        res.sendStatus(204);
+    }
+});
+
 app.get("/info", (req, res) => {
     res.send(`<p>Phonebook has info ${persons.length} people</p>
     <p>${new Date().toString()}</p>`);
